b61179468ebcc6c2efd4afcd93bca235aa93eced
claudioLoaiciga <loaiciga38@gmail.com>
2020-05-09 14:33:57 -0600

    parametros opcionales

--- routes/web.php
+++ routes/web.php
@@ -30,10 +30,13 @@ Route::get('/mostrar-fecha', function(){
      ));
 });
 
+Route::get('/pelicula/{titulo}/{year?}', function($titulo = 'No hay una pelicula seleccionada', $year = 2019){
+	return view('pelicula', array(
+		'titulo' => $titulo,
+		'year'   => $year
+	));
 
-Route::get('/pelicula/{titulo?}',function($titulo = 'No hay una pelicula seleccionada'){
-    return view('pelicula',array(
-        'titulo' => $titulo
-    ));
-
-});
+})->where(array(
+    'titulo' => '[a-zA-Z]+',
+    'year' => '[0-9]+'
+));
