7fbcb74d26d7ec18bf18e208e740bf375e529e78
claudioLoaiciga <loaiciga38@gmail.com>
2020-05-09 12:55:09 -0600

    Rutas basicas y Parametros de rutas

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * METODOS HTTP
+ * GET: Capturar datos
+ * POST: Guardar datos
+ * PUT: Actualizar recursos
+ * DELETE: Eliminar recursos
+ */
+
+Route::get('/mostrar-fecha', function(){
+    $titulo = "Estoy mostrando la fecha";
+     return view('mostrar-fecha',array(
+         'titulo' => $titulo
+     ));
+});
+
+
+Route::get('/pelicula/{titulo?}',function($titulo = 'No hay una pelicula seleccionada'){
+    return view('pelicula',array(
+        'titulo' => $titulo
+    ));
+
+});
