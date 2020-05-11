72223e001168ae7280c10a570071b743abdadd9e
claudioLoaiciga <loaiciga38@gmail.com>
2020-05-11 13:14:11 -0600

    enlaces en laravel

--- resources/views/pelicula/index.blade.php
+++ resources/views/pelicula/index.blade.php
@@ -4,3 +4,7 @@
 @if (isset($pagina))
    <h3> La pagina es {{$pagina}} </h3>
 @endif
+
+
+{{--<a href="{{action('PeliculaController@detalle')}}">ir al detalle</a>--}}
+<a href="{{route('detalle.pelicula')}}">ir al detalle</a>
